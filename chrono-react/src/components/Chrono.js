466245567c5fee2c1db7fc09383f191cd1753d87
Pablolo <pablo_olondriz@hotmail.com>
2020-04-22 19:50:50 +0200

    finished

--- chrono-react/src/components/Chrono.js
+++ chrono-react/src/components/Chrono.js
@@ -6,14 +6,13 @@ class Chrono extends Component {
     seconds: 0,
     mins: 0,
     hours: 0,
-    start: false,
-    stop: false,
-    reset: false,
     intervalId: undefined,
+    isButtonDisabled: false,
   }
-
+  
   init = () => {
     this.setState({
+      isButtonDisabled: !this.state.isButtonDisabled,
       intervalId: setInterval(() => {
         this.setState({
           seconds: this.state.seconds + 1,
@@ -39,18 +38,33 @@ class Chrono extends Component {
       clearInterval(this.state.intervalId);
       this.setState({
         intervalId: undefined,
+        isButtonDisabled: false,
       })
     }
+  }
+
+  reset = () => {
+    this.setState({
+      seconds: 0,
+      mins: 0,
+      hours: 0,
+      isButtonDisabled: false,
+    })
+    this.stop();
+  }
+
+  twoDigits(number) {
+    return (number / 100).toFixed(2).split('.')[1];
   }
 
   render() {
     const { seconds, mins, hours } = this.state;
     return (
       <div>
-        <p>{ hours }:{ mins }:{ seconds }</p>
-        <button onClick={this.init}>Start</button>
-        <button onClick={this.stop}>Stop</button>
-        <button>Reset</button>       
+        <p>{ this.twoDigits(hours) }:{ this.twoDigits(mins) }:{ this.twoDigits(seconds) }</p>
+        <button className='btns' onClick={this.init} disabled={this.state.isButtonDisabled}>Start</button>
+        <button className='btns' onClick={this.stop}>Stop</button>
+        <button className='btns' onClick={this.reset}>Reset</button>       
       </div>
     );
   }
